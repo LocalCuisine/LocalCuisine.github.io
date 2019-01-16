2d10bdea3090ffaa014a182983a0d2378456284b
adams52 <adams.b52@gmail.com>
2019-01-15 18:36:53 -0700

    Changed sheet to point to the correct one and modified the generator to use the appropriate variables

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@
 $(function() {
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '1UwUF_sDKAtD4NEutMFew8SmpbcThzskQEjCjCCJm_Cg' );
+    WTF.init( '1raHPdelMxASk_jpl-Ork9pE98memLa7KPie0iDGrPq0' );
 
 });
